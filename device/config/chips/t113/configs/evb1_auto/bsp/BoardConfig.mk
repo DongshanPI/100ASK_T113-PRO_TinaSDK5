358ff360472fa4aa0c2de7efa6081d44fa13954c
meihao <2776123335@qq.com>
2025-03-18 22:42:02 -0400

    add t11s3 wifi6 spi-nand-512m configuration files

---
+++ device/config/chips/t113/configs/evb1_auto/bsp/BoardConfig.mk
@@ -0,0 +1,5 @@
+LICHEE_KERN_DEFCONF:=config-5.4
+LICHEE_BUILDING_SYSTEM:=buildroot
+LICHEE_BR_VER:=201902
+LICHEE_BR_DEFCONF:=sun8iw20p1_longan_defconfig
+LICHEE_COMPILER_TAR=arm/gcc-linaro-5.3.1-2016.05-x86_64_arm-linux-gnueabi.tar.xz
